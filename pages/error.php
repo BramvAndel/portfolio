baf7cd7ac94593c7e75d53070f474c249e5abc2d
Bram van Andel <101711@glr.nl>
2024-12-14 14:21:22 +0100

    Add error page with custom styles and layout for 404 errors

---
+++ pages/error.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portfolio</title>
+    <link rel="stylesheet" href="../assets/css/general.css">
+    <link rel="stylesheet" href="../assets/css/fonts.css">
+    <link rel="stylesheet" href="../assets/css/error.css">
+    <script src="../assets/js/header.js" defer></script>
+    <script src="../assets/js/script.js" defer></script>
+    <script src="../assets/js/navbar.js" defer></script>
+</head>
+
+<body>
+    <?php include '../templates/header.tpl.php' ?>
+    <main>
+        <h1>404</h1>
+        <h2>I don’t think this is where you were ment to go</h2>
+    </main>
+    <div id="backgroundImage"></div>
+    <?php include '../templates/footer.tpl.php' ?>
+</body>
+
+</html>
